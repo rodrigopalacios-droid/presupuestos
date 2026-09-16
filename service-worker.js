35be9727bac016ef45905d20039b96f0e62f3b22
rodrigopalacios-droid <rodrigo.palacios@whitehawkusa.com>
2026-09-16 00:30:16 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-const CACHE='mi-bolsillo-v1'; const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg']; self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))); self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
